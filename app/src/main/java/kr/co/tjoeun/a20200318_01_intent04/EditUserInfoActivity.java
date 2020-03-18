4d6543649cdda90dd913b94a2f37125ac45792cf
potential20 <passion212@nate.com>
2020-03-18 09:48:05 +0900

    데이터바인딩 세팅 2개 화면

--- app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/EditUserInfoActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/EditUserInfoActivity.java
@@ -1,14 +1,21 @@
 package kr.co.tjoeun.a20200318_01_intent04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.tjoeun.a20200318_01_intent04.databinding.ActivityEditUserInfoBinding;
+
 public class EditUserInfoActivity extends AppCompatActivity {
+
+    ActivityEditUserInfoBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_info);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edit_user_info);
+
+
     }
 }
--- app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_01_intent04/MainActivity.java
@@ -1,14 +1,22 @@
 package kr.co.tjoeun.a20200318_01_intent04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kr.co.tjoeun.a20200318_01_intent04.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
+
     }
 }
